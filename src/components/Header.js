5871eb26b340c91bf04961390f87f43df2c335c5
ArtRosh <artem.rosh17@gmail.com>
2025-05-15 21:00:04 -0700

    set up App.js

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+
+function Header ({header}) {
+    console.log(header)
+    return(
+        <div>
+        <h1>{header.name}</h1>
+        </div>
+    )
+}
+
+
+export default Header
